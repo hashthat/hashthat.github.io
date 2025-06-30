d49800f2f4f6964d8fa8453e6cd51c01ba4507b2
hashthat <samalberts73@gmail.com>
2025-06-30 09:23:14 -0500

    ..foliopage

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nextjs-portfolio',
+  assetPrefix: '/nextjs-portfolio/',
+};
+
